16025122bdf52d96640c3d690a8746f8a75fcdab
AppSiDaL <gilielcrack343@gmail.com>
2025-01-07 10:08:49 -0600

    feat: add API route for serving resumes in multiple languages

---
+++ src/pages/api/resume/[lang].ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from "astro";
+import { promises as fs } from "fs";
+
+const pdfs = {
+  es: "src/assets/Resume-es.pdf",
+  en: "src/assets/Resume-en.pdf",
+};
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const lang = params.lang as "es" | "en";
+
+  const filePath = pdfs[lang];
+
+  const fileBuffer = await fs.readFile(filePath);
+
+  return new Response(fileBuffer, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": 'inline; filename="your-pdf-file.pdf"',
+    },
+  });
+};
+
+export function getStaticPaths() {
+  return [{ params: { lang: "es" } }, { params: { lang: "en" } }];
+}
